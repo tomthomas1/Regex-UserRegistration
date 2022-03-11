49b79023752a2bd7af0d491cd27271148e181313
tomthomas1 <tomthomas8688@gmail.com>
2022-03-11 23:59:02 +0530

    Junit to validate user entry and emotion

--- src/main/java/com/bridgelabz/EmotionCheck.java
+++ src/main/java/com/bridgelabz/EmotionCheck.java
@@ -1,5 +1,11 @@
 package com.bridgelabz;
-
+/**
+ *  We have created this method to check if it is Happy or sad test case
+ *  If it is happy then it will return Entry Successful.
+ *  3. If it contains sad then it will return Entry Failed
+ * @author Tom
+ *
+ */
 public class EmotionCheck {
 	public String analyseEmotion(String emotion) {
 		if (emotion.toLowerCase().contains("happy")) {
--- src/test/java/com/bridgelabz/EmotionTest.java
+++ src/test/java/com/bridgelabz/EmotionTest.java
@@ -3,15 +3,27 @@ package com.bridgelabz;
 import org.junit.Test;
 import org.junit.Assert;
 
+/**
+ *  We have created this class to test the emotion if happy or sad
+ * @author Tom
+ *
+ */
 public class EmotionTest {
 	EmotionCheck emotion = new EmotionCheck();
 
+	/**
+	 * If we created this test case to check if the user is happy.
+	 * If happy then it will be true
+	 */
     @Test
     public void givenMessage_WhenHappy_ReturnEntrySuccessful() {
         String actualResult = emotion.analyseEmotion("User is Happy");
         Assert.assertEquals("Entry Successful", actualResult);
     }
 
+    /**
+	 * If we created this test case to check if the user is sad.
+	 */
     @Test
     public void givenMessage_WhenNotProper_ReturnEntryFailed() {
         String actualResult = emotion.analyseEmotion("User is Sad");
